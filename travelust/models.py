7040a6cc477c02b8ce0b817e3e2136cd335f8a19
girissn <girissn@amazon.com>
2020-07-21 20:35:34 +0530

    Added migration files and ORM for travelust app

--- travelust/models.py
+++ travelust/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Destination(models.Model):
+    name = models.TextField(max_length=100)
+    price = models.IntegerField
+    desc = models.TextField(max_length=100)
+    img = models.ImageField(upload_to='pics')
--- travelust/views.py
+++ travelust/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from travelust.models import Destination
+
 
 def home(request):
-    return render(request, 'index.html')
+    dest1= Destination()
+    dest2 = Destination()
+    dest1.name = 'Bali'
+    dest1.desc = 'nice place'
+    dest1.price = 1000
+    dest2.name = 'indonesia'
+    dest2.desc = 'nicest place'
+    dest2.price = 500
+
+    return render(request, 'index.html', {'dest1': dest1, 'dest2': dest2})
